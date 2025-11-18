20fd310107a4578b9a13e755923ae6d7bfb6ba7a
michael stack <stack@duboce.com>
2025-11-17 20:05:36 -0800

    Add detailed trace logging for restore range key writes to diagnose missing keys issue

--- fdbclient/FileBackupAgent.actor.cpp
+++ fdbclient/FileBackupAgent.actor.cpp
@@ -4534,6 +4534,14 @@ struct RestoreRangeTaskFunc : RestoreFileTaskFuncBase {
 						tr->set(data[i].key.removePrefix(removePrefix.get()).withPrefix(addPrefix.get()),
 						        data[i].value);
 					}
+					
+					TraceEvent("FileRestoreRangeWrite")
+					    .suppressFor(60)
+					    .detail("RestoreUID", restore.getUid())
+					    .detail("KeysWritten", iend - start)
+					    .detail("FirstKey", printable(data[start].key))
+					    .detail("LastKey", printable(data[iend - 1].key))
+					    .detail("AddPrefix", printable(addPrefix.get()));
 
 					// Add to bytes written count
 					restore.bytesWritten().atomicOp(tr, txBytes, MutationRef::Type::AddValue);
